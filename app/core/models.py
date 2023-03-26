19f839e397f64fb097f94c3322513fb1bdc568fd
Vinicius Teixeira Saraiva <vinicius.ts.online@gmail.com>
2023-03-26 16:23:29 -0300

    implement user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,34 @@
-from django.db import models  # noqa
+"""
+Database models for the core app.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    """Manager for user profiles."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create a new user profile."""
+        if not email:
+            raise ValueError('Users must have an email address.')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
